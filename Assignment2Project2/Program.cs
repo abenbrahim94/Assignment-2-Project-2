a4f327ea6d1e7e97d93e6613624f247348d25886
abenbrahim94 <abenbrahim94@gmail.com>
2015-02-13 13:59:31 -0500

    Assignment2Project2Final

--- Assignment2Project2/Program.cs
+++ Assignment2Project2/Program.cs
@@ -10,6 +10,35 @@ namespace Assignment2Project2
     {
         static void Main(string[] args)
         {
+            // Variables
+            double HSGP; // High School Grade points
+            int ATS; // Admission Test Score
+            //Codes
+            Console.WriteLine("==========================Assignment2 project2=========================");
+            Console.WriteLine("College Application ");
+            Console.WriteLine("=======================================================");
+            Console.Write("\n");
+            Console.WriteLine("Insert your High School Grade Point Average:  ");
+            HSGP = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insert your Admission Test Score: ");
+            ATS = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            
+            if(HSGP >=3.0 && ATS >=60)
+            {
+                Console.WriteLine("Your Application Has Been Accepted!");
+            }
+            else if(HSGP <3.0 && ATS >=80)
+            {
+                Console.WriteLine("Your Application Has Been Accepted!");
+            }
+            else
+            {
+                Console.WriteLine("Your Application Has Been Rejected!");
+            }
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadKey();
         }
     }
 }
